5190a4ecc0979ca36041b444fe73726e8fd114fb
huanyuess <1037312774@qq.com>
2022-03-24 14:57:02 +0800

    Week5

--- scienceApp/views.py
+++ scienceApp/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import HttpResponse
 
 
 def science(request):
-    html='<html><body>科研基地</body></html>'
-    return HttpResponse(html)
+    return render(request, 'science.html')
+
